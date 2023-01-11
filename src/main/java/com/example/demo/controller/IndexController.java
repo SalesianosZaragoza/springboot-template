d9538714cea3afb5c26adb8e27ee488b53ece1d4
Gorka Sanz <gorka.sanz@salesianos.edu>
2023-01-11 11:54:38 +0100

    ignore info updated, log info student

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,19 +1,30 @@
 package com.example.demo.controller;
 
+
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.model.Student;
 
 @Controller
 public class IndexController {
+	
+	Logger l = org.apache.logging.log4j.LogManager.getLogger();
 
 	@GetMapping("/")
 	public ModelAndView goToIndexPage() {
 		ModelAndView modelAndView = new ModelAndView("index");
 		modelAndView.addObject("student", new Student());
 		return modelAndView;
+	}
+
+	@PostMapping("addStudentForm")
+	private String logStudentInfoAndSendToEndPage(Student student) {
+		l.info(student.toString());
+		return "end";
 	}
 
 }
---
+++ src/main/webapp/jsp/end.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>end</title>
+</head>
+<body>
+<div>usted ha terminado de trabajar</div>
+</body>
+</html>
