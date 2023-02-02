cdec5fb63302b84481b6c1501d4ac1bb479c9981
Gorka Sanz <gorka.sanz@salesianos.edu>
2023-02-02 09:43:32 +0100

    added delete method to student controller

--- src/main/java/com/example/demo/controller/StudentRestController.java
+++ src/main/java/com/example/demo/controller/StudentRestController.java
@@ -3,12 +3,15 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Student;
@@ -17,6 +20,7 @@ import com.example.demo.model.Student;
 @RequestMapping("/api/")
 public class StudentRestController {
 	List<Student> list = new ArrayList<>();
+	Logger l = org.apache.logging.log4j.LogManager.getLogger(IndexController.class);
 
 	@GetMapping
 	@RequestMapping(value = "/info")
@@ -31,6 +35,27 @@ public class StudentRestController {
 	@RequestMapping(value = "/save")
 	public ResponseEntity<List<Student>> saveStudent(@RequestBody Student student) {
 		list.add(student);
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+
+	@GetMapping
+	@RequestMapping(value = "/deleteAll")
+	public ResponseEntity<List<Student>> deleteAll() {
+		list.clear();
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+
+	@GetMapping(path = "/delete1")
+	public ResponseEntity<List<Student>> delete1(@RequestParam Integer id) {
+		l.info("delete1:" + id);
+		// list.remove(id.intValue() - 1);
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+
+	@GetMapping(path = "/delete2/{id}")
+	public ResponseEntity<List<Student>> delete2(@PathVariable("id") int id) {
+		l.info("delete2:" + id);
+		// list.remove(id.intValue() - 1);
 		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 
